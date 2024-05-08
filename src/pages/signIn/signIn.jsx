16524002d4708ebe1dfa836b1670d25391533374
Crypto Dev <experiencedtopdev@gmail.com>
2024-05-08 11:03:15 +0800

    finish implementation of sign up page

--- src/pages/signIn/signIn.jsx
+++ src/pages/signIn/signIn.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import SignInComponent from "../../components/signin/signIn";
 
 const SignIn = () => {
-	return <div></div>;
+	return (
+		<div>
+			<section id="signInComponent">
+				<SignInComponent />
+			</section>
+		</div>
+	);
 };
 
 export default SignIn;
